67648acbdf58de7f73f603ecff14b9210e34ab6f
Elliott-Vallee <116816433+Elliott-Vallee@users.noreply.github.com>
2022-11-24 09:54:42 -0500

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+function Bleu () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function Rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function Vert () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function Blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+basic.forever(function () {
+    if (pins.analogReadPin(AnalogPin.P0) < 450) {
+        Rouge()
+    } else if (pins.analogReadPin(AnalogPin.P0) > 550) {
+        Vert()
+    } else if (pins.analogReadPin(AnalogPin.P0) > 450 && pins.analogReadPin(AnalogPin.P0) < 550) {
+        Blanc()
+    }
+})
